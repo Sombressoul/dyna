13f452cbb9c67ad5edf57a1e399235e7ac21d0e4
Sombressoul <Sombressoul@gmail.com>
2024-02-11 11:37:05 +0300

    Update plotting fn

--- plot_dynaf.py
+++ plot_dynaf.py
@@ -1,30 +1,31 @@
 import torch
-import numpy as np
-import math
 import matplotlib.pyplot as plt
 
+from dynaf import DyNAFActivation
 
-def DyNAF(x, a, b, g, d):
-    return a * (
-        (1.0 / (1 + math.e ** (math.fabs(b) * (x - d - math.fabs(g)))))
-        - (1.0 / (1 + math.e ** (math.fabs(b) * (x - d + math.fabs(g)))))
-    )
+dynaf_activation = DyNAFActivation(passive=False)
+x = torch.linspace(-10, 10, 1000).unsqueeze(-1)  # Adding feature dimension
 
+params = torch.tensor(
+    [
+        (+1.00, +0.50, +1.00, +0.00),
+        (+0.15, +3.00, +5.50, +0.00),
+        (+0.20, +6.50, +0.25, +4.50),
+        (-1.55, +4.50, +0.15, -2.50),
+    ]
+).unsqueeze(-1)
 
-x = torch.linspace(-10, 10, 1000)
-
-params = [
-    (+1.00, +0.50, +1.00, +0.00),
-    (+0.15, +3.00, +5.50, +0.00),
-    (+0.20, +6.50, +0.25, +4.50),
-    (-1.55, +4.50, +0.15, -2.50),
-]
+_, nonlinearity, components = dynaf_activation.forward(
+    x, modes=params, return_components=True, return_nonlinearity=True
+)
 
 plt.figure(figsize=(10, 10))
-for i, (a, b, g, d) in enumerate(params, start=1):
-    y = DyNAF(x, a, b, g, d)
-    plt.plot(x.numpy(), y.numpy(), label=f"Set {i}: a={a}, b={b}, g={g}, d={d}")
-
+for i, component in enumerate(components):
+    plt.plot(
+        x.squeeze().numpy(),
+        component.squeeze().numpy(),
+        label=f"Set {i}",
+    )
 plt.title("DyNAF Components")
 plt.xlabel("Input (x)")
 plt.ylabel("Output")
@@ -32,13 +33,12 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-y = torch.zeros_like(x)
-for i, (a, b, g, d) in enumerate(params, start=1):
-    y = y + DyNAF(x, a, b, g, d)
-y = y + 1.0
-
 plt.figure(figsize=(10, 10))
-plt.plot(x.numpy(), y.numpy(), label=f"Resulting waveform")
+plt.plot(
+    x.squeeze().numpy(),
+    nonlinearity.squeeze().numpy(),
+    label="Resulting waveform",
+)
 plt.title("DyNAF Nonlinearity")
 plt.xlabel("Input (x)")
 plt.ylabel("Output")
@@ -46,10 +46,17 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-
 plt.figure(figsize=(10, 10))
-plt.plot(x.numpy(), x.numpy(), label=f"Original x")
-plt.plot(x.numpy(), (x * y).numpy(), label=f"Transformed x")
+plt.plot(
+    x.squeeze().numpy(),
+    x.squeeze().numpy(),
+    label="Original x",
+)
+plt.plot(
+    x.squeeze().numpy(),
+    (x * nonlinearity).squeeze().numpy(),
+    label="Transformed x",
+)
 plt.title("DyNAF Transformation")
 plt.xlabel("Input (x)")
 plt.ylabel("Output")
