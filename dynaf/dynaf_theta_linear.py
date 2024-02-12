e0382b0eb9d2f5ec214f946ced93022fd71711da
Sombressoul <Sombressoul@gmail.com>
2024-02-12 08:27:04 +0300

    Initial implementation of DyNAFThetaLinear

--- dynaf/dynaf_theta_linear.py
+++ dynaf/dynaf_theta_linear.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from typing import Tuple
+from typing import Tuple, Optional
 
 
 class DyNAFThetaLinear(nn.Linear):
@@ -9,20 +9,25 @@ class DyNAFThetaLinear(nn.Linear):
         self,
         in_features: int,
         out_features: int,
-        theta_in: int,
-        theta_out: int,
+        theta_modes_in: int,
+        theta_modes_out: int,
+        theta_full_features: Optional[bool] = True,
         **kwargs,
     ) -> None:
         super(DyNAFThetaLinear, self).__init__(in_features, out_features, **kwargs)
 
-        self.theta_in = theta_in
-        self.theta_out = theta_out
-        self.theta_out_quads = self.theta_out * 4
+        self.theta_modes_in = theta_modes_in
+        self.theta_modes_out = theta_modes_out
+        self.theta_feautures = out_features if theta_full_features else 1
+
+        # Calculate theta io size.
+        self.theta_in = self.theta_modes_in * in_features + out_features
+        self.theta_out = self.theta_modes_out * self.theta_feautures * 4
 
         # Define an additional linear layer for theta transformation.
         self.theta_transformation = nn.Linear(
-            in_features=out_features + self.theta_in,
-            out_features=self.theta_out_quads,
+            in_features=self.theta_in,
+            out_features=self.theta_out,
             bias=True,
         )
 
@@ -30,22 +35,23 @@ class DyNAFThetaLinear(nn.Linear):
 
     def forward(
         self,
-        x: torch.Tensor,
-        components: torch.Tensor,
+        x: torch.Tensor, # [batch, <unknown_dims>, in_features]
+        components: torch.Tensor, # [batch, <unknown_dims>, components_in, in_features]
     ) -> Tuple[torch.Tensor, torch.Tensor]:
         # Apply the parent class's linear transformation to x.
         x_transformed = super(DyNAFThetaLinear, self).forward(x)
 
-        # Flatten and expand components to match the shape of x_transformed.
-        components_expanded = ...
+        # Flatten: [batch, <unknown_dims>, components_in, in_features] -> [batch, <unknown_dims>, components_in*in_features]
+        components_flat = components.flatten(-2)
 
-        # Concat x_transformed & components_expanded to form the input (activation_state) to theta_transformation.
-        activation_state = ...
+        # Concat x_transformed with input components to form the activation state for Theta-transformation.
+        activation_state = torch.cat([x_transformed, components_flat], dim=-1)
 
         # Apply linear transformation to activation_state to transform it into param_quads.
         param_quads = self.theta_transformation(activation_state)
 
-        # Reshape param_quads to match the input modes shape of DyNAFActivation.
-        param_quads = ...
+        # Reshape param_quads to match the shape of input modes for DyNAFActivation.
+        param_quads_shape = [*x_transformed.shape[:-1], self.theta_modes_out, 4, self.theta_feautures]
+        param_quads = param_quads.reshape(param_quads_shape)
 
         return x_transformed, param_quads
