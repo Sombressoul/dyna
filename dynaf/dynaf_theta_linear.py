9bfa1d2485dfb9e14e82b926c4963b3e99981ddd
Sombressoul <Sombressoul@gmail.com>
2024-02-12 04:52:05 +0300

    Partial implementation of DyNAFThetaLinear

--- dynaf/dynaf_theta_linear.py
+++ dynaf/dynaf_theta_linear.py
@@ -7,18 +7,24 @@ from typing import Tuple
 class DyNAFThetaLinear(nn.Linear):
     def __init__(
         self,
-        *args,
+        in_features: int,
+        out_features: int,
         theta_in: int,
         theta_out: int,
         **kwargs,
     ) -> None:
-        super(DyNAFThetaLinear, self).__init__(*args, **kwargs)
+        super(DyNAFThetaLinear, self).__init__(in_features, out_features, **kwargs)
 
         self.theta_in = theta_in
         self.theta_out = theta_out
+        self.theta_out_quads = self.theta_out * 4
 
-        # Init sumodules.
-        ...
+        # Define an additional linear layer for theta transformation.
+        self.theta_transformation = nn.Linear(
+            in_features=out_features + self.theta_in,
+            out_features=self.theta_out_quads,
+            bias=True,
+        )
 
         pass
 
@@ -27,16 +33,19 @@ class DyNAFThetaLinear(nn.Linear):
         x: torch.Tensor,
         components: torch.Tensor,
     ) -> Tuple[torch.Tensor, torch.Tensor]:
-        # Apply linear transformation to x to transform it into x_transformed.
-        ...
+        # Apply the parent class's linear transformation to x.
+        x_transformed = super(DyNAFThetaLinear, self).forward(x)
 
-        # Concat x_transformed & components (previous neuromodulatory profile) into activation_state.
-        ...
+        # Flatten and expand components to match the shape of x_transformed.
+        components_expanded = ...
+
+        # Concat x_transformed & components_expanded to form the input (activation_state) to theta_transformation.
+        activation_state = ...
 
         # Apply linear transformation to activation_state to transform it into param_quads.
-        ...
+        param_quads = self.theta_transformation(activation_state)
 
-        # return x_transformed, param_quads.
-        ...
+        # Reshape param_quads to match the input modes shape of DyNAFActivation.
+        param_quads = ...
 
-        pass
+        return x_transformed, param_quads
