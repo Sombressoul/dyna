98aaedfe4dd6f1e3a0568a2a2d9c783a934f44a5
Sombressoul <Sombressoul@gmail.com>
2024-04-23 12:02:04 +0300

    Add imports for WeightsLib2DLite

--- dyna/__init__.py
+++ dyna/__init__.py
@@ -8,6 +8,7 @@ from dyna.theta_input import ThetaInput
 from dyna.theta_output import ThetaOutput
 from dyna.theta_linear import ThetaLinear
 from dyna.weights_lib_2d import WeightsLib2D
+from dyna.weights_lib_2d_lite import WeightsLib2DLite
 
 
 __all__ = [
@@ -22,4 +23,5 @@ __all__ = [
     "ThetaOutput",
     "ThetaLinear",
     "WeightsLib2D",
+    "WeightsLib2DLite",
 ]
