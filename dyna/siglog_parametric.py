f2d0d468bd81731111ac3fc34c0683b697ec8d96
Sombressoul <Sombressoul@gmail.com>
2024-08-23 02:12:35 +0300

    Add rough alpha derivative

--- dyna/siglog_parametric.py
+++ dyna/siglog_parametric.py
@@ -46,7 +46,7 @@ class SigLogParametric(torch.autograd.Function):
             dtype=torch.bool, device=input.device
         )
 
-        ctx.save_for_backward(input, mod, smoothing, smooth_grad)
+        ctx.save_for_backward(input, alpha, smoothing, smooth_grad)
 
         with torch.no_grad():
             x = SigLogParametric._real_forward(input, mod)
@@ -58,7 +58,9 @@ class SigLogParametric(torch.autograd.Function):
         ctx: torch.autograd.function.FunctionCtx,
         grad_output: torch.Tensor,
     ) -> torch.Tensor:
-        (input, mod, smoothing, smooth_grad) = ctx.saved_tensors
+        (input, alpha, smoothing, smooth_grad) = ctx.saved_tensors
+
+        mod = torch.e * alpha
 
         with torch.no_grad():
             if smooth_grad.item():
@@ -67,12 +69,18 @@ class SigLogParametric(torch.autograd.Function):
                 x_1 = SigLogParametric._real_forward(input + smoothing, mod)
                 x_2 = SigLogParametric._real_forward(input - smoothing, mod)
                 dx = (x_1 - x_2) / (2 * smoothing)
+                mod_1 = SigLogParametric._real_forward(input, mod + smoothing)
+                mod_2 = SigLogParametric._real_forward(input, mod - smoothing)
+                dmod = (mod_1 - mod_2) / (2 * smoothing)
             else:
                 # Exact derivative:
                 # f'(x) = 1.0 / (|x| + e*alpha)
                 dx = 1.0 / (input.abs() + mod)
+                mod_1 = SigLogParametric._real_forward(input, mod + SigLogParametric.smoothing)
+                mod_2 = SigLogParametric._real_forward(input, mod - SigLogParametric.smoothing)
+                dmod = (mod_1 - mod_2) / (2 * SigLogParametric.smoothing)
 
-        return grad_output * dx, None, None, None
+        return grad_output * dx, grad_output * dmod, None, None
 
 
 def siglog_parametric(
