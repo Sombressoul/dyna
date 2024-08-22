39ec548b86792c380c1b9788e55b02104232a5fa
Sombressoul <Sombressoul@gmail.com>
2024-08-23 02:23:35 +0300

    Fix SigLog Parametric alpha's smooth derivative

--- dyna/siglog_parametric.py
+++ dyna/siglog_parametric.py
@@ -60,24 +60,24 @@ class SigLogParametric(torch.autograd.Function):
     ) -> torch.Tensor:
         (input, alpha, smoothing, smooth_grad) = ctx.saved_tensors
 
-        mod = torch.e * alpha
+        get_mod = lambda alpha, smoothing = 0.0: torch.e * (alpha + smoothing)
 
         with torch.no_grad():
             if smooth_grad.item():
                 # Smooth symmetric derivative:
                 # f'(x) = (f(x+h) - f(x-h))/2h
-                x_1 = SigLogParametric._real_forward(input + smoothing, mod)
-                x_2 = SigLogParametric._real_forward(input - smoothing, mod)
+                x_1 = SigLogParametric._real_forward(input + smoothing, get_mod(alpha))
+                x_2 = SigLogParametric._real_forward(input - smoothing, get_mod(alpha))
                 dx = (x_1 - x_2) / (2 * smoothing)
-                mod_1 = SigLogParametric._real_forward(input, mod + smoothing)
-                mod_2 = SigLogParametric._real_forward(input, mod - smoothing)
+                mod_1 = SigLogParametric._real_forward(input, get_mod(alpha, +smoothing))
+                mod_2 = SigLogParametric._real_forward(input, get_mod(alpha, -smoothing))
                 dmod = (mod_1 - mod_2) / (2 * smoothing)
             else:
                 # Exact derivative:
                 # f'(x) = 1.0 / (|x| + e*alpha)
-                dx = 1.0 / (input.abs() + mod)
-                mod_1 = SigLogParametric._real_forward(input, mod + SigLogParametric.smoothing)
-                mod_2 = SigLogParametric._real_forward(input, mod - SigLogParametric.smoothing)
+                dx = 1.0 / (input.abs() + get_mod(alpha))
+                mod_1 = SigLogParametric._real_forward(input, get_mod(alpha, +SigLogParametric.smoothing))
+                mod_2 = SigLogParametric._real_forward(input, get_mod(alpha, -SigLogParametric.smoothing))
                 dmod = (mod_1 - mod_2) / (2 * SigLogParametric.smoothing)
 
         return grad_output * dx, grad_output * dmod, None, None
