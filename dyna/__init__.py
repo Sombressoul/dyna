50424c6fb55af72cdc4a0e9629ad0d9301604ced
Sombressoul <Sombressoul@gmail.com>
2024-10-20 10:44:56 +0300

    Change modules hierarchy

--- dyna/__init__.py
+++ dyna/__init__.py
@@ -1,11 +1,11 @@
+from dyna import functional
+
 from dyna.dynamic_conv2d import DynamicConv2D
 from dyna.exponential_warper_1d import ExponentialWarper1D
 from dyna.modulated_activation import ModulatedActivation
 from dyna.modulated_activation_bell import ModulatedActivationBell
 from dyna.modulated_activation_sine import ModulatedActivationSine
 from dyna.signal import SignalModular, SignalComponential
-from dyna.siglog import siglog
-from dyna.siglog_parametric import siglog_parametric
 from dyna.sparse_ensemble import SparseEnsemble
 from dyna.theta_input import ThetaInput
 from dyna.theta_output import ThetaOutput
@@ -13,8 +13,8 @@ from dyna.theta_linear import ThetaLinear
 from dyna.weights_lib_2d_dev import WeightsLib2DDev
 from dyna.weights_lib_2d import WeightsLib2D
 
-
 __all__ = [
+    "functional",
     "DynamicConv2D",
     "ExponentialWarper1D",
     "ModulatedActivation",
@@ -22,8 +22,6 @@ __all__ = [
     "ModulatedActivationSine",
     "SignalModular",
     "SignalComponential",
-    "siglog",
-    "siglog_parametric",
     "SparseEnsemble",
     "ThetaInput",
     "ThetaOutput",
---
+++ dyna/functional/__init__.py
@@ -0,0 +1,7 @@
+from dyna.functional.siglog import siglog
+from dyna.functional.siglog_parametric import siglog_parametric
+
+__all__ = [
+    "siglog",
+    "siglog_parametric",
+]
