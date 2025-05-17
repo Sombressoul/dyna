1e29222d42ab411b9544ad51997c71ba414a806f
Sombressoul <Sombressoul@gmail.com>
2025-05-17 11:03:59 +0300

    Improve dynamic weigths alpha

--- dyna/lib/weights_lib_2d_alpha.py
+++ dyna/lib/weights_lib_2d_alpha.py
@@ -12,8 +12,6 @@ class WeightsLib2DAlpha(nn.Module):
         context_length: int,
         context_rank: int = 4,
         context_use_bias: bool = False,
-        convolution_kernel_size: int = 3,
-        convolution_kernel_bias: bool = False,
         eps: float = 1.0e-5,
         dtype_weights: torch.dtype = torch.bfloat16,
     ) -> None:
@@ -22,8 +20,6 @@ class WeightsLib2DAlpha(nn.Module):
         self.output_shape = output_shape
         self.context_rank = context_rank
         self.context_use_bias = context_use_bias
-        self.convolution_kernel_size = convolution_kernel_size
-        self.convolution_kernel_bias = convolution_kernel_bias
         self.eps = eps
         self.dtype_weights = dtype_weights
 
@@ -41,39 +37,66 @@ class WeightsLib2DAlpha(nn.Module):
             bias=self.context_use_bias,
             dtype=self.dtype_weights,
         )
-        self.context_transform_norm = nn.LayerNorm(
-            normalized_shape=[ff_out_features],
-            elementwise_affine=True,
-            bias=False,
-            dtype=self.dtype_weights,
-        )
-        self.context_transform_mod_norm = nn.LayerNorm(
-            normalized_shape=[ff_out_features],
-            elementwise_affine=True,
-            bias=False,
-            dtype=self.dtype_weights,
-        )
-        self.mod_convolution = nn.Conv2d(
-            in_channels=4,
-            out_channels=4,
-            kernel_size=self.convolution_kernel_size,
-            stride=1,
-            padding=(self.convolution_kernel_size-1)//2,
-            dilation=1,
-            bias=self.convolution_kernel_bias,
-            padding_mode="replicate",
-            dtype=self.dtype_weights,
-        )
-        self.weights_base = nn.Parameter(
-            data=torch.nn.init.xavier_uniform_(
-                tensor=torch.empty(
-                    [
-                        *self.output_shape,
-                        2, # real, imag
-                    ],
-                    dtype=self.dtype_weights,
-                ),
-            ).contiguous(),
+        self.weights_static = nn.Parameter(
+            data=torch.cat(
+                [
+                    torch.cat(
+                        [
+                            torch.nn.init.kaiming_uniform_(
+                                tensor=torch.empty(
+                                    [
+                                        1, # base real
+                                        *self.output_shape,
+                                        1, # real
+                                    ],
+                                    dtype=self.dtype_weights,
+                                ),
+                            ),
+                            torch.nn.init.kaiming_uniform_(
+                                tensor=torch.empty(
+                                    [
+                                        1, # base imag
+                                        *self.output_shape,
+                                        1, # imag
+                                    ],
+                                    dtype=self.dtype_weights,
+                                ),
+                            ),
+                        ],
+                        dim=-1
+                    ),
+                    torch.cat(
+                        [
+                            torch.nn.init.uniform_(
+                                tensor=torch.empty(
+                                    [
+                                        1, # scale real
+                                        *self.output_shape,
+                                        1, # real
+                                    ],
+                                    dtype=self.dtype_weights,
+                                ),
+                                a=+self.eps - 1.0,
+                                b=-self.eps + 1.0,
+                            ),
+                            torch.nn.init.uniform_(
+                                tensor=torch.empty(
+                                    [
+                                        1, # scale imag
+                                        *self.output_shape,
+                                        1, # imag
+                                    ],
+                                    dtype=self.dtype_weights,
+                                ),
+                                a=-self.eps,
+                                b=+self.eps,
+                            ),
+                        ],
+                        dim=-1,
+                    ),
+                ],
+                dim=0,
+            ).contiguous()
         )
         self.weights_mod = nn.Parameter(
             data=torch.nn.init.xavier_uniform_(
@@ -98,11 +121,10 @@ class WeightsLib2DAlpha(nn.Module):
         x = x if x.dtype == self.dtype_weights else x.to(self.dtype_weights)
 
         x_transformed = self.context_transform(x)
-        x_transformed = self.context_transform_norm(x_transformed)
         x_gate = self.context_transform_gate(x)
         x_gate = x_gate * torch.nn.functional.sigmoid(x_gate)
         mod = x_transformed * x_gate
-        mod = self.context_transform_mod_norm(mod)
+        mod = mod / mod.abs().mean(dim=-1).sqrt().unsqueeze(-1) # mean sqrt norm
         mod = torch.reshape(
             input=mod,
             shape=[
@@ -117,31 +139,24 @@ class WeightsLib2DAlpha(nn.Module):
             mod[..., 0:self.output_shape[0]], 
             mod[..., self.output_shape[0]::],
         )
-        mod = self.mod_convolution(mod)
 
-        A = self.weights_base.unsqueeze(0).repeat([mod.shape[0], *[1]*(len(mod.shape)-1)])
+        A = self.weights_static.unsqueeze(0).repeat([mod.shape[0], 1, *[1]*(len(mod.shape)-1)])
         B = mod[::, 0:2:, ...].permute([0, 2, 3, 1])
-        mod_weights = torch.cat(
+        mod_scaled = torch.cat(
             tensors=[
-                (A[..., 0] * B[..., 0] - A[..., 1] * B[..., 1]).unsqueeze(-1),
-                (A[..., 0] * B[..., 1] - A[..., 1] * B[..., 0]).unsqueeze(-1),
+                (A[::, 1, ..., 0] * B[..., 0] - A[::, 1, ..., 1] * B[..., 1]).unsqueeze(-1),
+                (A[::, 1, ..., 0] * B[..., 1] - A[::, 1, ..., 1] * B[..., 0]).unsqueeze(-1),
             ],
             dim=-1,
         )
+        mod_weights = A[::, 0, ...] + mod_scaled
         mod_proj = mod[::, 2::, ...].permute([0, 2, 3, 1])
-        mods = torch.cat(
-            [
-                mod_weights.unsqueeze(1),
-                mod_proj.unsqueeze(1),
-            ],
-            dim=1,
-        )
 
-        proj_den = torch.sqrt(mods[::, 1, ..., 0] ** 2 + mods[::, 1, ..., 1] ** 2 + self.eps)
-        theta_cos = mods[::, 1, ..., 0] / proj_den
-        theta_sin = mods[::, 1, ..., 1] / proj_den
+        proj_den = torch.sqrt(mod_proj[::, ..., 0] ** 2 + mod_proj[::, ..., 1] ** 2 + self.eps)
+        theta_cos = mod_proj[::, ..., 0] / proj_den
+        theta_sin = mod_proj[::, ..., 1] / proj_den
         weights = (
-            mods[::, 0, ..., 0] * theta_cos + mods[::, 0, ..., 1] * theta_sin
+            mod_weights[::, ..., 0] * theta_cos + mod_weights[::, ..., 1] * theta_sin
         )
 
         x = weights if weights.dtype == input_dtype else weights.to(input_dtype)
