8b5a74428a99ea07c4db6583c363783222b646f5
Sombressoul <Sombressoul@gmail.com>
2025-05-20 00:30:03 +0300

    Fix missing import for BGNLayer

--- dyna/module/__init__.py
+++ dyna/module/__init__.py
@@ -1,8 +1,10 @@
+from dyna.module.backward_gradient_normalization import BGNLayer
 from dyna.module.exponential_warper_1d import ExponentialWarper1D
 from dyna.module.dynamic_conv2d import DynamicConv2D
 from dyna.module.dynamic_conv2d_alpha import DynamicConv2DAlpha
 
 __all__ = [
+    "BGNLayer",
     "ExponentialWarper1D",
     "DynamicConv2D",
     "DynamicConv2DAlpha",
