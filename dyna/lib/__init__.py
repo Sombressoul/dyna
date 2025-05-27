6d806ed9db4ce7ee29d0b4161772adacfd8f6b7e
Sombressoul <Sombressoul@gmail.com>
2025-05-28 02:59:10 +0300

    Add WeightsLib2DDelta to init

--- dyna/lib/__init__.py
+++ dyna/lib/__init__.py
@@ -4,6 +4,7 @@ from dyna.lib.weights_lib_2d_alpha import WeightsLib2DAlpha
 from dyna.lib.weights_lib_2d_beta import WeightsLib2DBeta
 from dyna.lib.weights_lib_2d_beta_second_order import WeightsLib2DBetaSecondOrder
 from dyna.lib.weights_lib_2d_gamma import WeightsLib2DGamma
+from dyna.lib.weights_lib_2d_delta import WeightsLib2DDelta
 
 __all__ = [
     "WeightsLib2DDev",
@@ -12,4 +13,5 @@ __all__ = [
     "WeightsLib2DBeta",
     "WeightsLib2DBetaSecondOrder",
     "WeightsLib2DGamma",
+    "WeightsLib2DDelta",
 ]
