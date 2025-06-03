343cbfe0eac6af89c107560f85ea3636a5c92933
Sombressoul <Sombressoul@gmail.com>
2025-06-03 08:45:11 +0300

    Add import for WeightsLib2DMobius

--- dyna/lib/__init__.py
+++ dyna/lib/__init__.py
@@ -1,7 +1,9 @@
 from dyna.lib.weights_lib_2d import WeightsLib2D
 from dyna.lib.weights_lib_2d_delta import WeightsLib2DDelta
+from dyna.lib.weights_lib_2d_mobius import WeightsLib2DMobius
 
 __all__ = [
     "WeightsLib2D",
     "WeightsLib2DDelta",
+    "WeightsLib2DMobius",
 ]
