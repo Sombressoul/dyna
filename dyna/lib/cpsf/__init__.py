ac40198a663dede612e519bba3bfeac74cceb49e
sombressoul <sombressoul@gmail.com>
2025-09-01 21:30:56 +0300

    update cpsf init

--- dyna/lib/cpsf/__init__.py
+++ dyna/lib/cpsf/__init__.py
@@ -1,17 +1,62 @@
+from dyna.lib.cpsf.context import (
+    CPSFContext,
+    CPSFGeometryCache,
+    CPSFDerivedCache,
+)
+from dyna.lib.cpsf.contribution_store_facade import CPSFContributionStoreFacade
+from dyna.lib.cpsf.core import CPSFCore
+from dyna.lib.cpsf.errors import (
+    UnitDirectionError,
+    InactiveIndexError,
+    NumericalError,
+    ZeroMaterializationError,
+    SnapshotViolationError,
+)
+from dyna.lib.cpsf.module import CPSFModule
+from dyna.lib.cpsf.projection import CPSFProjection
 from dyna.lib.cpsf.contribution_store import CPSFContributionStore
 from dyna.lib.cpsf import functional
 from dyna.lib.cpsf.structures import (
-    CPSFIndexLike,
+    CPSFChunkPolicy,
+    CPSFConsistency,
     CPSFContributionField,
     CPSFContributionSet,
     CPSFContributionStoreIDList,
+    CPSFDTypes,
+    CPSFIndexLike,
+    CPSFIntegrationPolicy,
+    CPSFLatticeSumPolicy,
 )
 
 __all__ = [
+    # Main components.
+    "CPSFContext",
+    "CPSFContributionStore",
+    "CPSFContributionStoreFacade",
+    "CPSFCore",
+    "CPSFDerivedCache",
+    "CPSFGeometryCache",
+    "CPSFModule",
+    "CPSFProjection",
+
+    # Structures.
+    "CPSFChunkPolicy",
+    "CPSFConsistency",
     "CPSFContributionField",
     "CPSFContributionSet",
-    "CPSFContributionStore",
     "CPSFContributionStoreIDList",
+    "CPSFDTypes",
     "CPSFIndexLike",
+    "CPSFIntegrationPolicy",
+    "CPSFLatticeSumPolicy",
+
+    # Subcomponents.
     "functional",
+
+    # Errors.
+    "UnitDirectionError",
+    "InactiveIndexError",
+    "NumericalError",
+    "ZeroMaterializationError",
+    "SnapshotViolationError",
 ]
