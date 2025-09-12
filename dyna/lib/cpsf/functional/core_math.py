psi_over_offsets(): broadcasting fix

dyna/lib/cpsf/functional/core_math.py
@@ -1097,7 +1097,7 @@ def psi_over_offsets(
     dz_re = dz.real.unsqueeze(-2) + off_r.unsqueeze(-3)
     dz_im = dz.imag.unsqueeze(-2)
     dzb = torch.complex(dz_re, dz_im)
-    dd_b = dd.unsqueeze(-2)
+    dd_b = dd.unsqueeze(-2).expand_as(dzb)
     w = iota(dzb, dd_b)
     qv = q(w, Rext.unsqueeze(-3), sigma_par, sigma_perp)
     eta = rho(qv, q_max=q_max)