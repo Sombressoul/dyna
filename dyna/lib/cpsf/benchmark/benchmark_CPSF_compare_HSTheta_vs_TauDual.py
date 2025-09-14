8f6f44f76d467c6a3424d803168016949163ca87
sombressoul <sombressoul@gmail.com>
2025-09-14 07:34:43 +0300

    update HSTheta vs TauDual benchmark

--- dyna/lib/cpsf/benchmark/benchmark_CPSF_compare_HSTheta_vs_TauDual.py
+++ dyna/lib/cpsf/benchmark/benchmark_CPSF_compare_HSTheta_vs_TauDual.py
@@ -1,6 +1,6 @@
 # dyna/lib/cpsf/benchmark/benchmark_CPSF_compare_HSTheta_vs_TauDual.py
 # Run examples:
-# > python -m dyna.lib.cpsf.benchmark.benchmark_CPSF_compare_HSTheta_vs_TauDual --N 4 --M 128 --S 64 --batch 16 --dtype c64 --device cpu --K 7 --quad_nodes 7
+# > python -m dyna.lib.cpsf.benchmark.benchmark_CPSF_compare_HSTheta_vs_TauDual --N 4 --M 128 --S 64 --batch 16 --dtype c64 --device cpu --K 7 --quad_nodes 7 --phase_scale 0.5
 
 import argparse
 import torch
