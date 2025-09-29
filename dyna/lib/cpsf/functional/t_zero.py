bf1cb266fbf0a7f17d8eb374d34a4caedd0dcb7e
sombressoul <sombressoul@gmail.com>
2025-09-29 14:48:49 +0300

    add T_Zero

---
+++ dyna/lib/cpsf/functional/t_zero.py
@@ -0,0 +1,58 @@
+import torch
+
+from dyna.lib.cpsf.functional.core_math import delta_vec_d
+
+def T_Zero(
+    z: torch.Tensor,  # [B,N] (complex)
+    z_j: torch.Tensor,  # [B,M,N] (complex)
+    vec_d: torch.Tensor,  # vec_d: [B,N] (complex)
+    vec_d_j: torch.Tensor,  # vec_d_j: [B,M,N] (complex)
+    T_hat_j: torch.Tensor,  # T_hat_j: [B,M,S] (complex)
+    alpha_j: torch.Tensor,  # alpha_j: [B,M] (real)
+    sigma_par: torch.Tensor,  # sigma_par: [B,M] (real)
+    sigma_perp: torch.Tensor,  # sigma_perp: [B,M] (real)
+) -> torch.Tensor:
+    # ============================================================
+    # BASE
+    # ============================================================
+    device = z.device
+    dtype_r = z.real.dtype
+    tiny = torch.finfo(dtype_r).tiny
+
+    # ============================================================
+    # MAIN
+    # ============================================================
+    # Broadcast
+    B, M, N = vec_d_j.shape
+    z = z.unsqueeze(1).expand(B, M, N)
+    vec_d = vec_d.unsqueeze(1).expand(B, M, N)
+
+    # Constants
+    PI = torch.tensor(torch.pi, dtype=dtype_r, device=device)
+
+    # Common
+    x = z - z_j  # [B,M,N] complex
+    sigma_q_inv = torch.reciprocal(sigma_perp)  # [B,M]
+    sigma_p_inv = torch.reciprocal(sigma_par)  # [B,M]
+
+    # ============================================================
+    # ZERO-FRAME
+    # ============================================================
+    # q_pos: [B,M]
+    x_norm_sq = (x.real * x.real + x.imag * x.imag).sum(dim=-1)  # [B,M]
+    inner_re = (vec_d_j.real * x.real + vec_d_j.imag * x.imag).sum(dim=-1)  # [B,M]
+    inner_im = (vec_d_j.real * x.imag - vec_d_j.imag * x.real).sum(dim=-1)  # [B,M]
+    inner_abs_sq = inner_re * inner_re + inner_im * inner_im  # [B,M]
+    q_pos = sigma_q_inv * x_norm_sq + (sigma_p_inv - sigma_q_inv) * inner_abs_sq  # [B,M]
+    A_pos = torch.exp(-PI * q_pos)  # [B,M]
+
+    # A_dir: [B,M]
+    delta_d = delta_vec_d(vec_d, vec_d_j)  # [B,M,N] complex
+    delta_d_norm_sq = (delta_d.real * delta_d.real + delta_d.imag * delta_d.imag).sum(dim=-1)  # [B,M]
+    A_dir = torch.exp(-PI * sigma_q_inv.clamp(tiny) * delta_d_norm_sq)  # [B,M]
+
+    # Gain
+    gain_zero = alpha_j * A_pos * A_dir  # [B,M]
+    T_zero = (gain_zero.unsqueeze(-1) * T_hat_j).sum(dim=1)  # [B,S]
+
+    return T_zero
